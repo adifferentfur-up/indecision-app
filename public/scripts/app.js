ffc98dcbaabba0a797ffeff93afccaa0aeb7daf4
Alex Garcia <adifferentfur.up@gmail.com>
2020-12-01 04:12:37 -0800

    using babel

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'JSX from app.js!'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
